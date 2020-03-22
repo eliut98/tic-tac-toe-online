da56d992675c4f6daed280293b9b136201168f10
Roberth <roberthgon18@gmail.com>
2020-03-23 00:14:26 +0100

    Check winner

--- src/index.js
+++ src/index.js
@@ -22,6 +22,7 @@ class Board {
   setPosition(x, y, value) {
     if (!this.positions[x][y]) {
       this.positions[x][y] = value;
+      this.checkWinner(x, y);
       this.drawPosition(x, y, value);
       // Toggle turn
       sessionStorage.setItem("turn", value === "X" ? "O" : "X");
@@ -31,6 +32,75 @@ class Board {
     document.querySelector(
       `[data-id="${x}${y}"]`
     ).innerHTML = `<span class="${value}">${value}</span>`;
+  }
+  checkWinner(x, y) {
+    let winner;
+
+    function check(arr, value) {
+      return arr.join().match(new RegExp(`${value}`, "g"));
+    }
+
+    // Horizontal
+    let hO = check(this.positions[x], "O");
+    let hX = check(this.positions[x], "X");
+
+    if (hX && hX.length === 3) winner = "X";
+    if (hO && hO.length === 3) winner = "O";
+
+    // Vertical
+    let vertical = [];
+    for (let i = 0; i < 3; i++) {
+      vertical[i] = this.positions[i][y];
+    }
+
+    let vX = check(vertical, "X");
+    let vO = check(vertical, "O");
+
+    if (vX && vX.length === 3) winner = "X";
+    if (vO && vO.length === 3) winner = "O";
+
+    // Diagonal
+    let tbr = [];
+    if (x === y) {
+      for (let i = 0; i < 3; i++) {
+        tbr[i] = this.positions[i][i];
+      }
+    }
+
+    let tbrX = check(tbr, "X");
+    let tbrO = check(tbr, "O");
+
+    if (tbrX && tbrX.length === 3) winner = "X";
+    if (tbrO && tbrO.length === 3) winner = "O";
+
+    console.log(this.positions);
+
+    let tbl = new Array(3);
+    switch ([x, y].join("")) {
+      case "02":
+        updateTbl(this.positions);
+        break;
+      case "11":
+        updateTbl(this.positions);
+        break;
+      case "20":
+        updateTbl(this.positions);
+        break;
+    }
+
+    function updateTbl(positions) {
+      tbl[0] = positions[0][2];
+      tbl[1] = positions[1][1];
+      tbl[2] = positions[2][0];
+    }
+
+    console.log(tbl);
+
+    if (winner) this.showWinner(winner);
+  }
+
+  showWinner(winner) {
+    alert(`${winner} wins!!!`);
   }
 }
 
@@ -44,9 +114,6 @@ cols.forEach(col => {
     const index = this.dataset.id;
     const [x, y] = index;
     const turn = sessionStorage.getItem("turn");
-
-    b.setPosition(x, y, turn);
-
-    console.log(b.positions);
+    b.setPosition(Number(x), Number(y), turn);
   });
 });
