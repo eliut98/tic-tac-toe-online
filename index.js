64c5c4394da4d15ae2f5521804b43927ad208243
Roberth <roberthgon18@gmail.com>
2020-03-28 21:18:52 +0100

    Show the winner

--- index.js
+++ index.js
@@ -5,23 +5,100 @@ const io = require("socket.io")(http);
 
 app.use(express.static("public"));
 
+let positions = [];
+
+function init() {
+  for (let i = 0; i < 3; i++) positions[i] = new Array();
+}
+
+function checkWinner(x, y) {
+  let winner;
+
+  function check(arr, value) {
+    return arr.join().match(new RegExp(`${value}`, "g"));
+  }
+
+  // Horizontal
+  let hO = check(positions[x], "O");
+  let hX = check(positions[x], "X");
+
+  if (hX && hX.length === 3) winner = "X";
+  if (hO && hO.length === 3) winner = "O";
+
+  // Vertical
+  let vertical = [];
+  for (let i = 0; i < 3; i++) {
+    vertical[i] = positions[i][y];
+  }
+
+  let vX = check(vertical, "X");
+  let vO = check(vertical, "O");
+
+  if (vX && vX.length === 3) winner = "X";
+  if (vO && vO.length === 3) winner = "O";
+
+  // Diagonal
+  let tbr = [];
+  if (x === y) {
+    for (let i = 0; i < 3; i++) {
+      tbr[i] = positions[i][i];
+    }
+  }
+
+  let tbrX = check(tbr, "X");
+  let tbrO = check(tbr, "O");
+
+  if (tbrX && tbrX.length === 3) winner = "X";
+  if (tbrO && tbrO.length === 3) winner = "O";
+
+  let tbl = new Array(3);
+  switch ([x, y].join("")) {
+    case "02":
+      updateTbl(positions);
+      break;
+    case "11":
+      updateTbl(positions);
+      break;
+    case "20":
+      updateTbl(positions);
+      break;
+  }
+
+  function updateTbl(positions) {
+    tbl[0] = positions[0][2];
+    tbl[1] = positions[1][1];
+    tbl[2] = positions[2][0];
+  }
+
+  let tblX = check(tbl, "X");
+  let tblO = check(tbl, "O");
+
+  if (tblX && tblX.length === 3) winner = "X";
+  if (tblO && tblO.length === 3) winner = "O";
+
+  return winner;
+}
+
 io.on("connection", function(socket) {
   const symbol = {
     O: "X",
     X: "O"
   };
-  socket.on("drawPosition", data => {
-    io.emit("drawPosition", data);
-  });
   socket.on("symbol", data => {
     socket.broadcast.emit("symbol", symbol[data]);
-    io.emit("turn", { turn: data });
+    init();
+    io.emit("turn", { turn: data, positions });
   });
-  socket.on("turn", ({ turn, positions }) => {
-    io.emit("turn", { turn, positions });
-  });
-  socket.on("message", data => {
-    io.emit("message", data);
+  socket.on("turn", ({ turn, x, y, value }) => {
+    if (!positions[x][y]) {
+      positions[x][y] = value;
+      io.emit("drawPosition", { x, y, value });
+      io.emit("turn", { turn, positions });
+      if (checkWinner(x, y)) {
+        const winner = checkWinner(x, y);
+        io.emit("message", winner);
+      }
+    }
   });
   socket.on("replay", data => {
     io.emit("replay", data);
--- public/index.js
+++ public/index.js
@@ -1,5 +1,5 @@
 const socket = io();
-let positions = new Array();
+let positions;
 
 socket.on("turn", ({ turn, positions }) => {
   const symbol = sessionStorage.getItem("symbol");
@@ -14,10 +14,8 @@ class Board {
     document.getElementById("board").innerHTML = "";
     this.board = document.getElementById("board");
   }
-
   draw() {
     for (let i = 0; i < 3; i++) {
-      positions[i] = new Array();
       this.board.innerHTML += `<div class="row" data-id="${i}"></div>`;
       for (let j = 0; j < 3; j++) {
         const col = `<div class="col" data-id="${i}${j}"></div>`;
@@ -27,90 +25,20 @@ class Board {
   }
   setPosition(x, y, value, symbol) {
     // Check if the column is empty and if it's my turn
-    if (!positions[x][y] && value === symbol) {
-      positions[x][y] = value;
-      socket.emit("drawPosition", { x, y, value });
+    if (value === symbol) {
       // Toggle turn
       socket.emit("turn", {
         turn: value === "X" ? "O" : "X",
-        positions
+        x,
+        y,
+        value
       });
-      this.checkWinner(x, y);
     }
   }
   drawPosition(x, y, value) {
     document.querySelector(
       `[data-id="${x}${y}"]`
     ).innerHTML = `<span class="${value}">${value}</span>`;
-  }
-  checkWinner(x, y) {
-    let winner;
-
-    function check(arr, value) {
-      return arr.join().match(new RegExp(`${value}`, "g"));
-    }
-
-    // Horizontal
-    let hO = check(positions[x], "O");
-    let hX = check(positions[x], "X");
-
-    if (hX && hX.length === 3) winner = "X";
-    if (hO && hO.length === 3) winner = "O";
-
-    // Vertical
-    let vertical = [];
-    for (let i = 0; i < 3; i++) {
-      vertical[i] = positions[i][y];
-    }
-
-    let vX = check(vertical, "X");
-    let vO = check(vertical, "O");
-
-    if (vX && vX.length === 3) winner = "X";
-    if (vO && vO.length === 3) winner = "O";
-
-    // Diagonal
-    let tbr = [];
-    if (x === y) {
-      for (let i = 0; i < 3; i++) {
-        tbr[i] = positions[i][i];
-      }
-    }
-
-    let tbrX = check(tbr, "X");
-    let tbrO = check(tbr, "O");
-
-    if (tbrX && tbrX.length === 3) winner = "X";
-    if (tbrO && tbrO.length === 3) winner = "O";
-
-    let tbl = new Array(3);
-    switch ([x, y].join("")) {
-      case "02":
-        updateTbl(positions);
-        break;
-      case "11":
-        updateTbl(positions);
-        break;
-      case "20":
-        updateTbl(positions);
-        break;
-    }
-
-    function updateTbl(positions) {
-      tbl[0] = positions[0][2];
-      tbl[1] = positions[1][1];
-      tbl[2] = positions[2][0];
-    }
-
-    let tblX = check(tbl, "X");
-    let tblO = check(tbl, "O");
-
-    if (tblX && tblX.length === 3) winner = "X";
-    if (tblO && tblO.length === 3) winner = "O";
-
-    if (winner) socket.emit("message", `!${winner} HA GANADO!`);
-
-    // if (count === 9) socket.emit("message", `!EMPATE!`);
   }
 }
 
@@ -147,13 +75,16 @@ cols.forEach(col => {
   });
 });
 
-socket.on("drawPosition", ({ x, y, value }) => {
-  b.drawPosition(x, y, value);
-});
+socket.on("drawPosition", ({ x, y, value }) => b.drawPosition(x, y, value));
 
 socket.on("replay", () => window.location.reload());
 
-socket.on("message", message => {
-  document.getElementById("message").innerHTML = message;
+socket.on("message", winner => {
+  const symbol = sessionStorage.getItem("symbol");
+  document.getElementById("message").innerHTML =
+    winner === symbol ? "¡HAS GANADO!" : `¡HA GANADO ${winner}!`;
+  document.getElementById("message").classList.add("win");
+  document.querySelector("#board").remove();
   document.querySelector("button").style.display = "block";
+  document.querySelector("button").classList.add("win");
 });
